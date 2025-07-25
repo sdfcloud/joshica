8f1803d8f27d04c7998d0b39bd04416022ada3a2
RustyGoldCoin <josh@sdfcloud.co>
2025-07-25 12:54:28 +0000

    Moved `demo` to `docs` to serve github pages.

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path');
 const app = express();
 const port = 8000;
 
-app.use(express.static(path.join(__dirname)));
-app.use(express.static(path.join(__dirname, '..', 'dist')));
+app.use(express.static(path.join(__dirname, '.', 'docs')));
+app.use(express.static(path.join(__dirname, '.', 'dist')));
 
 app.listen(port, () => {
   console.log(`Dev server running at http://localhost:${port}`);
